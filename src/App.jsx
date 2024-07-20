da45149186ff09eaae32d4dbe644e0849107364f
abdul8840 <abdul14941naseer@gmail.com>
2024-07-20 10:58:01 +0530

    create song player ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Player from "./components/Player"
 import Sidebar from "./components/Sidebar"
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
-      <div className="h-90% flex">
+      <div className="h-[90%] flex">
         <Sidebar />
       </div>
+      <Player />
     </div>
   )
 }
