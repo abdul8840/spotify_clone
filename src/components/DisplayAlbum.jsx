e2f79d2d9f4d392f25bb540fd3efa322f2a81fb1
abdul8840 <abdul14941naseer@gmail.com>
2024-07-22 11:24:39 +0530

    add playwithid functionality to select and play any song

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom"
 import Navbar from "./Navbar"
 import { albumsData, assets, songsData } from "../assets/assets";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
     const {id} = useParams();
     const albumData = albumsData[id];
+    const { playWithId } = useContext(PlayerContext);
   return (
     <>
       <Navbar />
@@ -31,7 +34,7 @@ const DisplayAlbum = () => {
       </div>
       <hr />
       {songsData.map((item, index) => (
-        <div key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff26] cursor-pointer ">
+        <div onClick={() => playWithId(item.id)} key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff26] cursor-pointer ">
           <p className="text-white">
             <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
             <img src={item.image} alt="" className="inline w-10 mr-5" />
