57763c5c56fbf6105a9490ec1f3de11b5b1ac3e5
abdul8840 <abdul14941naseer@gmail.com>
2024-07-23 11:22:16 +0530

    added project to mongodb database and cloudinary and create route for adding and listing song

--- spotify_backend/server.js
+++ spotify_backend/server.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //initializing route
+app.use('/api/song', songRouter)
 
 app.get('/', (req, res) => res.send("Api working"))
 
