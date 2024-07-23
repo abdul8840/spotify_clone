2872314c6c5515e72cacbb4b99d71849cd66134c
abdul8840 <abdul14941naseer@gmail.com>
2024-07-23 12:10:57 +0530

    create api with functionality to add song to database

--- spotify_backend/src/controllers/songController.js
+++ spotify_backend/src/controllers/songController.js
@@ -1,5 +1,35 @@
-const addSong = async (req, res) => {
+import { v2 as cloudinary } from 'cloudinary';
+import songModel from '../models/songModel.js'
 
+const addSong = async (req, res) => {
+    try {
+        const name = req.body.name;
+        const desc = req.body.desc;
+        const album = req.body.album;
+        const audioFile = req.files.audio[0];
+        const imageFile = req.files.image[0];
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video"});
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"});
+        const duration = `${Math.floor(audioUpload.duration/60)}:${Math.floor(audioUpload.duration%60)}`
+
+        const songData = {
+            name,
+            desc,
+            album,
+            file: audioUpload.secure_url,
+            image: imageUpload.secure_url,
+            duration,
+        }
+
+        const song = songModel(songData);
+        await song.save();
+
+        res.json({success: true, message: "Song added"})
+
+    } catch (error) {
+        res.json({success: false})
+        console.log(error)
+    }
 }
 
 const listSong = async (req, res) => {
