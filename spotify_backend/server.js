eab206a877ea583466d5d8887347c8c8411ec6bd
abdul8840 <abdul14941naseer@gmail.com>
2024-07-23 10:52:28 +0530

    setup backend to run and test api

--- spotify_backend/server.js
+++ spotify_backend/server.js
@@ -1,1 +1,16 @@
-// This is Server.js File
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//initializing route
+
+app.get('/', (req, res) => res.send("Api working"))
+
+app.listen(port, () => console.log(`Server running on ${port}`))
