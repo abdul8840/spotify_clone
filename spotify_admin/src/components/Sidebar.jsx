33f988cbfd22d229f86f736f8ecc4e7269858329
abdul8840 <abdul14941naseer@gmail.com>
2024-07-24 11:43:53 +0530

    Create and add ui with functionality to add song from admin panel

--- spotify_admin/src/components/Sidebar.jsx
+++ spotify_admin/src/components/Sidebar.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
-      <img src={assets.logo} alt="" className="mt-5 w-[max(10vw, 100px) hidden sm:block] " />
-      <img src={assets.logo_small} alt="" className="mt-5 w-[max(5vw, 40px) mr-5 sm:hidden] block" />
+      <img src={assets.logo} alt="" className="mt-5 w-[max(10vw, 100px)] hidden sm:block " />
+      <img src={assets.logo_small} alt="" className="mt-5 w-[max(5vw, 40px)] mr-5 sm:hidden block" />
 
       <div className="flex flex-col gap-5 mt-10">
 
